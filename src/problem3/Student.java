93e09b8c79f67690d613d875c450c8b76e0ea1d2
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-27 13:01:11 +0530

    constructor created : Student()

--- src/problem3/Student.java
+++ src/problem3/Student.java
@@ -9,4 +9,9 @@ package problem3;
 public class Student {
     private String name;
     private int rollNo;
+
+    public Student(String s, int r) {
+        name = s;
+        rollNo = r;
+    }
 }
