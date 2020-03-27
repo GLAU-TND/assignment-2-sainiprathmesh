a7c3f08e4ab1864cc376a0ad913420ffbf75cced
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-27 14:13:58 +0530

    object creation and variable decleration

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,9 +5,20 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyCircularQueue myCircularQueue;
+        myCircularQueue = new MyCircularQueue();
+        System.out.println("Enter the size of Student list");
+        int size = sc.nextInt();
+        sc.nextLine();
 
     }
 }
