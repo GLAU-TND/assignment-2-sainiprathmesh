ea1264b79cc868dd3e9154bb9c546fc9815a6850
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-27 13:02:09 +0530

    getter ans setter for all the field

--- src/problem3/Student.java
+++ src/problem3/Student.java
@@ -14,4 +14,20 @@ public class Student {
         name = s;
         rollNo = r;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
 }
