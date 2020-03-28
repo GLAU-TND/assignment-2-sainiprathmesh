1c5dc3a471e2e589df5b787a0743f36e3d78e754
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-28 17:25:16 +0530

    constructor created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,8 +6,24 @@
  */
 package problem1.node;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TreeNode {
     private TreeNode left;
     private TreeNode right;
     private int data;
+
+    public TreeNode() {
+        System.out.print("Enter integer value : ");
+        try {
+            data = new Scanner(System.in).nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input");
+            e.getMessage();
+            System.exit(-1);
+        }
+        left = right = null;
+
+    }
 }
