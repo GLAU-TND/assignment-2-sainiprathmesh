26c8f627015c4788e167def45331ec85f7840823
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-28 17:29:54 +0530

    constructor created

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -10,4 +10,11 @@ package problem4.myqueue;
 public class MyQueue {
     private Node front, end, tmp;
     private int size;
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+    }
 }
