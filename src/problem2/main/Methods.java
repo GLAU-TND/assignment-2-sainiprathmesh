a7393548b30cc99d3d0b871d83a5e506dabf2330
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-28 17:35:53 +0530

    class created : Methods

---
+++ src/problem2/main/Methods.java
@@ -0,0 +1,10 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: prathmesh
+ *   Date: 28/03/20
+ *   Time: 5:35 PM
+ */
+package problem2.main;
+
+public class Methods {
+}
--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,10 +5,38 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        Methods mthds = new Methods();
+        MyQueue q = new MyQueue();
+        mthds.setPre(q);
+
+        //setting up the root
+        m.setRoot();
+
+        //inserting into the tree
+        for (int i = 0; i < 5; i++) {
+            m.insert(m.getRoot());
+        }
+
+        //method to print preorder Successor of given Node
+        /*
+        preorder method in
+         */
+        mthds.preOrder(m.getRoot());
+        q.queuePrint(q);
+        System.out.print("Enter value of which you want to find preorder Successor : ");
+        System.out.println(q.getSuccessor(sc.nextInt()));
 
     }
 }
