b59925cf7c681fbda2015d82cf41d39b61c96536
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-03-28 17:37:10 +0530

    constructor created

--- src/problem2/main/Methods.java
+++ src/problem2/main/Methods.java
@@ -11,4 +11,9 @@ import problem4.myqueue.MyQueue;
 public class Methods {
     private MyQueue pre, post;
 
+    public Methods() {
+
+        pre = new MyQueue();
+        post = new MyQueue();
+    }
 }
